2be9835d4e725435c68b5c5503605e29fe602daf
itsSoheil <soheil.samadi79@gmail.com>
2024-02-06 09:22:11 +0330

    Create Documents

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+const tabData = [
+  {
+    id: 1,
+    title: "ITEM 1",
+    content: "Authorize the user data ✅",
+  },
+  {
+    id: 2,
+    title: "ITEM 2",
+    content: "Redirect user to cart page 🛒",
+  },
+  {
+    id: 3,
+    title: "ITEM 3",
+    content: "Create new payment for the user 💰",
+  },
+];
+
+function App() {
+return <h1>Tab Component : starter file</h1>
+}
+
+export default App;
